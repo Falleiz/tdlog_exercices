b864a63f18ad2890c94359a5ec5818108a014186
Falleiz <153763392+Falleiz@users.noreply.github.com>
2024-09-27 08:55:25 +0100

    1er commit de l'exo1

--- exo1/test_exo1.py
+++ exo1/test_exo1.py
@@ -1,11 +1,8 @@
 import unittest
-
-from exo1 import Item
+from exo1.exo import Item
 
 
 class Exo1Test(unittest.TestCase):
-
     def test_item_construction(self):
         item = Item(10, 20)
-
         self.assertEqual(20, item.weight)
